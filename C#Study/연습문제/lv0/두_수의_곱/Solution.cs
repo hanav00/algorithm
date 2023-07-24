2c8e97a9a991dc996a599063d3d7373d0865af88
HanaV <123611673+hanav00@users.noreply.github.com>
2023-07-24 10:50:18 +0900

    Update Solution.cs

--- C#Study/연습문제/lv0/두_수의_곱/Solution.cs
+++ C#Study/연습문제/lv0/두_수의_곱/Solution.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int num1, int num2) {
-        if (0 <= num1 && num1 <= 100 && 0<= num2 && num2 <= 100) return num1 * num2;
+        if (0 < num1 && num1 <= 100 && 0< num2 && num2 <= 100) return num1 * num2;
         else {
             try {
                 throw new Exception("invalid param");
