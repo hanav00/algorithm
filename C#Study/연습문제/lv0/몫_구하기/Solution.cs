762575d320ebdb8ec0cd102a40bd5917b90acbfd
HanaV <123611673+hanav00@users.noreply.github.com>
2023-07-24 10:54:38 +0900

    Create Solution.cs

---
+++ C#Study/연습문제/lv0/몫_구하기/Solution.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        if (0 < num1 && num1 <= 100 && 0 < num2 && num2 <= 100) return num1 / num2;
+        else {
+            try{
+                throw new Exception ("invalid param");
+            }
+            catch {
+                Console.WriteLine("범위 벗어남");
+                return -1;
+            }
+        }
+    }
+}
