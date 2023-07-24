8f2adb94afa85675df533e747a9d6c74f48b9f43
HanaV <123611673+hanav00@users.noreply.github.com>
2023-07-24 15:59:49 +0900

    Create Solution.cs

---
+++ C#Study/연습문제/lv0/가장_큰_수_찾기/Solution.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] array) {
+
+        int max = array.Max();
+        int maxIndex = Array.IndexOf(array, max);
+        
+        int[] answer = new int[2]{max, maxIndex};
+        
+        return answer;
+    }
+}
