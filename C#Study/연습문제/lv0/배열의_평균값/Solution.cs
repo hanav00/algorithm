71c325e929f339735da556e29d59f1a90a0ec2f9
HanaV <123611673+hanav00@users.noreply.github.com>
2023-07-24 12:55:34 +0900

    Create Solution.cs

---
+++ C#Study/연습문제/lv0/배열의_평균값/Solution.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        int sum = 0;
+        
+        for (int i=0; i<numbers.Length; i++) {
+            sum += numbers[i];
+        }
+        return (double)sum / numbers.Length;
+    }
+}
