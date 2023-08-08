f2bb28c92b5d091b6c5f03ad3dbfc24770fcdf8e
HanaV <123611673+hanav00@users.noreply.github.com>
2023-08-08 10:35:21 +0900

    Create Solution.cs

---
+++ C#Study/연습문제/lv0/문자열 뒤집기/Solution.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string) {
+        string answer = "";
+        for (int i=0; i<my_string.Length; i++)
+        {
+            answer += my_string.Substring(my_string.Length - 1 -i, 1);
+        }
+        return answer;
+    }
+}
