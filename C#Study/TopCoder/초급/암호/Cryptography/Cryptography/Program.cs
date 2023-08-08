e57884aef53ae0f4824353f156c679be9feea0df
HanaV <123611673+hanav00@users.noreply.github.com>
2023-08-08 12:16:27 +0900

    Add files via upload

---
+++ C#Study/TopCoder/초급/암호/Cryptography/Cryptography/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Cryptography
+{
+    class Cryptography
+    {
+        static void Main(String[] args)
+        {
+            /*
+             테스트 케이스
+             int[] numbers = new int[] { 1, 2, 3 };
+             return: 12
+
+            int[] numbers = new int[] { 1, 3, 2, 1, 1, 3 };
+             return: 36
+
+            int[] numbers = new int[] { 1000, 999, 998, 997, 996, 995 };
+             return: 986074810223904000
+
+            int[] numbers = new int[] { 1, 1, 1, 1 };
+             return: 2
+             */
+
+            int[] numbers = new int[] { 1000, 999, 998, 997, 996, 995 };
+            long answer = Solution(numbers);
+            Console.WriteLine(answer);
+        }
+
+        private static long Solution(int[] numbers)
+        {
+            long mul = 1;
+            foreach (int n in numbers)
+            {
+                mul *= n;
+            }
+
+            long max = mul;
+
+            for (int i = 0; i<numbers.Length; i++)
+            {
+                if (mul / numbers[i] * (numbers[i] + 1) > max) max = mul / numbers[i] * (numbers[i] + 1);
+            }
+
+            return max;
+        }
+    }
+}
