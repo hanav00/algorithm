9bd6f19273fa741148f0c1b29a24a35c20a61ef2
HanaV <123611673+hanav00@users.noreply.github.com>
2023-08-20 08:52:03 +0900

    Create Solution.cs

---
+++ C#Study/연습문제/lv0/2차원으로 만들기/Solution.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int[,] solution(int[] num_list, int n) {
+        int rows = num_list.Length / n;
+        int[,] answer = new int[rows, n];
+        
+        int index = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < n; j++) {
+                answer[i, j] = num_list[index++];
+            }
+        }
+        
+        return answer;
+    }
+}
