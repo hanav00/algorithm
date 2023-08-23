6f5a685daaecfa672ddcea0fd1b42591ad775f31
HanaV <123611673+hanav00@users.noreply.github.com>
2023-08-23 10:46:44 +0900

    Create Solution.cs

---
+++ C#Study/연습문제/lv0/배열 원소의 길이/Solution.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int[] solution(string[] strlist) {
+        int[] answer = new int[strlist.Length];
+        for (int i=0; i<answer.Length; i++){
+            answer[i] = strlist[i].Length;
+        }
+        return answer;
+    }
+}
