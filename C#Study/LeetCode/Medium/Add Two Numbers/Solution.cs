dc27abddc3be80aabd77239c3bf4e7431359832b
HanaV <123611673+hanav00@users.noreply.github.com>
2023-10-03 10:51:52 +0900

    Create Solution.cs

---
+++ C#Study/LeetCode/Medium/Add Two Numbers/Solution.cs
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+using System;
+class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode dummyHead = new ListNode();
+        ListNode current = dummyHead;
+        int carry = 0; 
+
+        while (l1 != null || l2 != null)
+        {
+            int x = (l1 != null) ? l1.val : 0; 
+            int y = (l2 != null) ? l2.val : 0; 
+
+            int sum = x + y + carry; 
+            carry = sum / 10; 
+            int digit = sum % 10; 
+
+            current.next = new ListNode(digit);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next; 
+        }
+
+        if (carry > 0)
+        {
+            current.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
