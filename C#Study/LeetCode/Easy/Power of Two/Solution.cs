28bcc6d8ea008d0a6d6e211563575f8f1ec0c231
HanaV <123611673+hanav00@users.noreply.github.com>
2023-10-18 09:14:54 +0900

    Create Solution.cs

---
+++ C#Study/LeetCode/Easy/Power of Two/Solution.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public bool IsPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        while (n > 1){
+            if (n%2 != 0) return false;
+            else n = n/2;
+        }
+        return true;
+    }
+}
